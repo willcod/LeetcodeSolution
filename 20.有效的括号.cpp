c801e5aa6a764ecd9a3ef292061178939cc5857d
willcod <william.wow@icloud.com>
2020-08-28 07:23:19 +0800

    added 20

---
+++ 20.有效的括号.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=cpp
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  bool isValid(string s) {
+    stack<char> stk;
+    for (char c : s) {
+      if (c == '(' || c == '[' || c == '{') {
+        stk.push(c);
+      }
+      else {
+            if(stk.empty()) return false;
+            if(c == ')' && stk.top() != '(') return false;
+            if(c == '}' && stk.top() != '{') return false;
+            if(c == ']' && stk.top() != '[') return false;
+            stk.pop();
+      }
+    }
+    return stk.empty();
+  }
+};
+// @lc code=end
