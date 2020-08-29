9a2acf4a351f0652ede549a7e21b06fced7bbc8f
willcod <william.wow@icloud.com>
2020-08-30 07:36:59 +0800

    added 214 cpp

---
+++ source/214.最短回文串.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=214 lang=cpp
+ *
+ * [214] 最短回文串
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  string shortestPalindrome(string s) {
+    int len = s.length();
+    if (len < 2) return s;
+
+    int longest = 1, start, end;
+
+    for (int i = 0; i <= len/2;) {
+      start = end = i;
+      while (end < len - 1 && s[end] == s[end+1]) end++;
+      i = end + 1;
+      while (end < len - 1 && start > 0 && s[end+1] == s[start-1]) {
+        end++;
+        start--;
+      }
+
+      if (start == 0 && longest < end - start + 1) {
+        longest = end - start + 1;
+      }
+    }
+
+    string remain = s.substr(longest, len - longest);
+    reverse(remain.begin(), remain.end());
+    return remain + s;
+  }
+};
+// @lc code=end
