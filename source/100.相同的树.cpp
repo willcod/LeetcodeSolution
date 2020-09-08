3a645933acca72661fb5baca55faed593fe1e963
willcod <william.wow@icloud.com>
2020-09-08 22:37:15 +0800

    update

--- source/100.相同的树.cpp
+++ source/100.相同的树.cpp
@@ -13,17 +13,16 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
- public:
-  bool isSameTree(TreeNode* p, TreeNode* q) {
-    if (p == NULL || q == NULL) return p == q;
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p || !q) return p == q;
 
-    return (p->val == q->val && isSameTree(p->left, q->left) &&
-            isSameTree(p->right, q->right));
-  }
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
 };
 // @lc code=end
+
