c4a195f7892246184da4b01bde4eca4da0c644fc
willcod <william.wow@icloud.com>
2020-09-30 17:33:26 +0800

    update

--- source/208.实现-trie-前缀树.cpp
+++ source/208.实现-trie-前缀树.cpp
@@ -7,47 +7,43 @@
 // @lc code=start
 class Trie {
     bool _isEnd;
-    Trie* _branch[26];
-public:
-    /** Initialize your data structure here. */
-    Trie() {
-        _isEnd = false;
-        memset(_branch, NULL, sizeof(_branch));
-    }
+    vector<Trie *> _next;
 
+   public:
+    /** Initialize your data structure here. */
+    Trie() : _isEnd(false), _next(26, NULL) {}
     ~Trie() {
-        for (auto n : _branch) {
+        for (auto n : _next) {
             if (n) delete n;
         }
     }
-    
     /** Inserts a word into the trie. */
     void insert(string word) {
-        auto n = this;
-        for (auto c : word) {
-            if (!n->_branch[c-'a'])
-                n->_branch[c-'a'] = new Trie();
-            n = n->_branch[c-'a'];
+        auto t = this;
+        for (char c : word) {
+            if (t->_next[c - 'a'] == NULL) t->_next[c - 'a'] = new Trie();
+            t = t->_next[c - 'a'];
         }
-        n->_isEnd = true;
+        t->_isEnd = true;
     }
-    
+
     /** Returns if the word is in the trie. */
     bool search(string word) {
-        auto n = this;
-        for (auto c : word) {
-            n = n->_branch[c-'a'];
-            if (!n) return false;
+        auto t = this;
+        for (char c : word) {
+            if (t->_next[c - 'a'] == NULL) return false;
+            t = t->_next[c - 'a'];
         }
-        return n->_isEnd;
+        return t->_isEnd;
     }
-    
-    /** Returns if there is any word in the trie that starts with the given prefix. */
+
+    /** Returns if there is any word in the trie that starts with the given
+     * prefix. */
     bool startsWith(string prefix) {
-        auto n = this;
-        for (auto c : prefix) {
-            n = n->_branch[c-'a'];
-            if (!n) return false;
+        auto t = this;
+        for (char c : prefix) {
+            if (t->_next[c - 'a'] == NULL) return false;
+            t = t->_next[c - 'a'];
         }
         return true;
     }
@@ -61,4 +57,3 @@ public:
  * bool param_3 = obj->startsWith(prefix);
  */
 // @lc code=end
-
