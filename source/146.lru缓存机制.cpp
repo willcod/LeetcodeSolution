fa96f2922c7b226bb86eb536026edee71ef5d36b
willcod <william.wow@icloud.com>
2020-10-04 20:32:34 +0800

    update

--- source/146.lru缓存机制.cpp
+++ source/146.lru缓存机制.cpp
@@ -10,28 +10,28 @@ class LRUCache {
     LRUCache(int capacity) : _capacity(capacity) {}
 
     int get(int key) {
-        auto it = _keyMap.find(key);
-        if (it == _keyMap.end()) return -1;
-
-        _keyList.erase(it->second.second);
-        _keyList.push_back(key);
-        _keyMap[key].second = --_keyList.end();
-
-        return it->second.first;
+        auto it = _hashMap.find(key);
+        if (it != _hashMap.end()) {
+            _keyList.erase(it->second.second);
+            _keyList.push_back(key);
+            _hashMap[key].second = --_keyList.end();
+            return it->second.first;
+        }
+        return -1;
     }
 
     void put(int key, int value) {
         if (get(key) != -1) {
-            _keyMap[key].first = value;
+            _hashMap[key].first = value;
             return;
         }
 
-        if (_keyMap.size() < _capacity) {
+        if (_hashMap.size() < _capacity) {
             insert(key, value);
         } else {
             int removeKey = _keyList.front();
             _keyList.pop_front();
-            _keyMap.erase(removeKey);
+            _hashMap.erase(removeKey);
             insert(key, value);
         }
     }
@@ -39,13 +39,13 @@ class LRUCache {
    private:
     void insert(int key, int value) {
         _keyList.push_back(key);
-        _keyMap[key] = {value, --_keyList.end()};
+        _hashMap[key] = {value, --_keyList.end()};
     }
 
    private:
     int _capacity;
     list<int> _keyList;
-    unordered_map<int, pair<int, list<int>::iterator>> _keyMap;
+    unordered_map<int, pair<int, list<int>::iterator>> _hashMap;
 };
 
 /**
