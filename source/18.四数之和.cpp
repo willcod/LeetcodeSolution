7dd0d3f2d9f9de77617b61bbf8623e9bde3f9753
willcod <william.wow@icloud.com>
2020-10-05 16:54:30 +0800

    update

--- source/18.四数之和.cpp
+++ source/18.四数之和.cpp
@@ -3,44 +3,45 @@
  *
  * [18] 四数之和
  */
-
+#include "cpp_includes.h"
 // @lc code=start
 class Solution {
-public:
+   public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> res;
+        if (nums.size() < 4) return {};
         sort(nums.begin(), nums.end());
-
+        vector<vector<int>> res;
         for (int i = 0; i < nums.size(); i++) {
             int targetof3 = target - nums[i];
             for (int j = i + 1; j < nums.size(); j++) {
                 int targetof2 = targetof3 - nums[j];
 
-                int f = j + 1;
-                int b = nums.size() - 1;
+                int left = j + 1;
+                int right = nums.size() - 1;
 
-                while (f < b) {
-                    int sum = nums[f] + nums[b];
-                    if (sum == targetof2) {
-                        res.push_back({nums[i], nums[j], nums[f], nums[b]});
-                        while (f < b && nums[f+1] == nums[f]) f++;
-                        while (f < b && nums[b-1] == nums[b]) b--;
-                        f++;
-                        b--;
-                    } else if (sum > targetof2) {
-                        b--;
-                    } else {
-                        f++;
-                    }
+                while (left < right) {
+                    int sumof2 = nums[left] + nums[right];
+                    if (sumof2 == targetof2) {
+                        res.push_back(
+                            {nums[i], nums[j], nums[left], nums[right]});
+
+                        while (left < right && nums[right] == nums[right - 1])
+                            right--;
+                        while (left < right && nums[left] == nums[left + 1])
+                            left++;
+                        right--;
+                        left++;
+                    } else if (sumof2 > targetof2)
+                        right--;
+                    else
+                        left++;
                 }
-
-                while ( j + 1 < nums.size() && nums[j] == nums[j+1]) j++;
+                while (j + 1 < nums.size() && nums[j] == nums[j + 1]) j++;
             }
-            while (i + 1 < nums.size() && nums[i] == nums[i+1]) i++;
+            while (i + 1 < nums.size() && nums[i] == nums[i + 1]) i++;
         }
 
         return res;
     }
 };
 // @lc code=end
-
