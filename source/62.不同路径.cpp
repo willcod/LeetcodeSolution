1f26b8a9bfc5cbdd2e83554f08fe7bee0fb7a8e1
william.zh2018 <william.zh2018@outlook.com>
2020-10-07 17:23:06 +0800

    added 75 and 541

--- source/62.不同路径.cpp
+++ source/62.不同路径.cpp
@@ -6,20 +6,20 @@
 #include "cpp_includes.h"
 // @lc code=start
 class Solution {
- public:
-  int uniquePaths(int m, int n) {
-    vector<vector<int>> dp(m, vector<int>(n, 0));
-
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        if (i == 0 || j == 0) {
-          dp[i][j] = 1;
-        } else {
-          dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+   public:
+    int uniquePaths(int m, int n) {
+        vector f(m, vector(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    f[i][j] = 1;
+                } else {
+                    f[i][j] = f[i - 1][j] + f[i][j - 1];
+                }
+            }
         }
-      }
+
+        return f[m - 1][n - 1];
     }
-    return dp[m - 1][n - 1];
-  }
 };
 // @lc code=end
