9d13951363c960b77ea0d98bf1487a0490bd75c3
willcod <william.wow@icloud.com>
2020-10-11 21:00:59 +0800

    update

--- source/191.位-1-的个数.cpp
+++ source/191.位-1-的个数.cpp
@@ -8,12 +8,13 @@
 class Solution {
    public:
     int hammingWeight(uint32_t n) {
-        int sum = 0;
+        int count = 0;
         while (n) {
-            n &= (n - 1);
-            sum++;
+            count++;
+            n &= n - 1;
         }
-        return sum;
+
+        return count;
     }
 };
 // @lc code=end
