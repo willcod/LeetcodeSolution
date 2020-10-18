c3f357c08cbd23a60ccc0bf4420599bab11058cc
willcod <william.wow@icloud.com>
2020-10-19 06:46:27 +0800

    added 101

---
+++ source/101.对称二叉树.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=cpp
+ *
+ * [101] 对称二叉树
+ */
+#include "cpp_includes.h"
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+   public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+
+        return isMirror(root->left, root->right);
+    }
+
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+
+        return left->val == right->val && isMirror(left->left, right->right) &&
+               isMirror(left->right, right->left);
+    }
+};
+// @lc code=end
