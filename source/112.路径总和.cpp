40b58d0dcb9ca28b1ad026dd2c6b2ef72f11b497
willcod <william.wow@icloud.com>
2021-01-01 22:07:23 +0800

    added 112

---
+++ source/112.路径总和.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=cpp
+ *
+ * [112] 路径总和
+ */
+#include "cpp_includes.h"
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+   public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (!root) return false;
+        if (!root->left && !root->right) {
+            return sum == root->val;
+        }
+
+        return hasPathSum(root->left, sum - root->val) ||
+               hasPathSum(root->right, sum - root->val);
+    }
+};
+// @lc code=end
