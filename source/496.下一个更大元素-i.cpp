5a9ca27a41f6efb3e645dd1f4a3fe3db17a85b6d
willcod <william.wow@icloud.com>
2021-01-01 16:04:13 +0800

    daily

--- source/496.下一个更大元素-i.cpp
+++ source/496.下一个更大元素-i.cpp
@@ -6,18 +6,18 @@
 #include "cpp_includes.h"
 // @lc code=start
 class Solution {
-   public:
+public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res(nums1.size(), -1);
-        unordered_map<int, int> hash2;
+        unordered_map<int, int> nums2pos;
         for (int i = 0; i < nums2.size(); i++) {
-            hash2[nums2[i]] = i;
+            nums2pos[nums2[i]] = i;
         }
 
+        vector<int> res(nums1.size(), -1);
         for (int i = 0; i < nums1.size(); i++) {
-            int start = hash2[nums1[i]];
-            for (int j = start + 1; j < nums2.size(); j++) {
-                if (nums1[i] < nums2[j]) {
+            int start = nums2pos[nums1[i]];
+            for (int j = start+1; j < nums2.size(); j++) {
+                if (nums2[j] > nums1[i]) {
                     res[i] = nums2[j];
                     break;
                 }
@@ -27,3 +27,4 @@ class Solution {
     }
 };
 // @lc code=end
+
