4a51a0bed4f321087a496f72776d50b8a18a5e6c
willcod <william.wow@icloud.com>
2021-03-07 22:15:38 +0800

    update

--- source/33.搜索旋转排序数组.cpp
+++ source/33.搜索旋转排序数组.cpp
@@ -8,11 +8,13 @@
 class Solution {
    public:
     int search(vector<int>& nums, int target) {
+        if (nums.empty()) return -1;
         int left = 0;
         int right = nums.size() - 1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
+
             if (nums[mid] == target) return mid;
 
             if (nums[mid] >= nums[left]) {
@@ -22,14 +24,13 @@ class Solution {
                     left = mid + 1;
                 }
             } else {
-                if (target <= nums[right] && target > nums[mid]) {
+                if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
                 }
             }
         }
-
         return -1;
     }
 };
