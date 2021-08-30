b548384c8aa08b99f6512356d34f561f666ced67
willcod <william.wow@icloud.com>
2021-08-30 11:09:50 +0800

    回文链表-堆栈方法

--- source/234.回文链表.cpp
+++ source/234.回文链表.cpp
@@ -10,48 +10,31 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
-public:
-    bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) return true;
+ public:
+  bool isPalindrome(ListNode* head) {
+    stack<ListNode*> stk;
 
-        auto pre = head, slow = head, fast = head;
-        while (fast && fast->next) {
-            fast = fast->next->next;
-            pre = slow;
-            slow = slow->next;
-        }
+    ListNode* cur = head;
 
-        pre->next = nullptr;
-        auto l1 = head;
-        auto l2 = reverseList(slow);
-        
-        if (!fast) slow = slow->next;
-
-        while (l1 && l2) {
-            if (l1->val != l2->val) return false;
-            l1 = l1->next;
-            l2 = l2->next;
-        }
-        return true;
+    while (cur) {
+      stk.push(cur);
+      cur = cur->next;
     }
 
-    ListNode* reverseList(ListNode*head) {
-        if (!head || !head->next) return head;
+    cur = head;
+    while (cur) {
+      if (stk.top()->val != cur->val) return false;
 
-        ListNode* pre = nullptr;
-        auto cur = head;
-        while(cur) {
-            auto next = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = next;
-        }
-        return pre;
+      stk.pop();
+      cur = cur->next;
     }
+    return true;
+  }
 };
 // @lc code=end
-
