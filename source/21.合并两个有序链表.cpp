7a194087dabe520149aac288cd21ee13a388ecdb
willcod <william.wow@icloud.com>
2021-08-30 09:01:28 +0800

    [merge two link lists]-iterative method

--- source/21.合并两个有序链表.cpp
+++ source/21.合并两个有序链表.cpp
@@ -18,16 +18,23 @@
 class Solution {
    public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (!l1) return l2;
-        if (!l2) return l1;
+        ListNode dummy;
+        ListNode* zip = &dummy;
 
-        if (l1->val < l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l2->next, l1);
-            return l2;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                zip->next = l1;
+                l1 = l1->next;
+            } else {
+                zip->next = l2;
+                l2 = l2->next;
+            }
+            zip = zip->next;
         }
+
+        zip->next = l1 ? l1 : l2;
+
+        return dummy.next;
     }
 };
 // @lc code=end
