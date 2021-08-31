3e41424fa88e9953141b81da018dea0d54ea7b43
willcod <william.wow@icloud.com>
2021-08-31 08:48:42 +0800

    二叉树的最大深度-cpp and python

--- source/104.二叉树的最大深度.py
+++ source/104.二叉树的最大深度.py
@@ -13,7 +13,8 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        if not root: return 0
+        if root is None: return 0
+
         return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
 # @lc code=end
 
