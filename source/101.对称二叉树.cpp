219e8d7139deb5daa3266a34154804af18e92c26
willcod <william.wow@icloud.com>
2021-08-31 09:27:16 +0800

    对称二叉树

--- source/101.对称二叉树.cpp
+++ source/101.对称二叉树.cpp
@@ -11,24 +11,26 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-   public:
-    bool isSymmetric(TreeNode* root) {
-        if (!root) return true;
-        return isMirror(root->left, root->right);
-    }
+ public:
+  bool isSymmetric(TreeNode* root) {
+    if (!root) return true;
 
-    bool isMirror(TreeNode* left, TreeNode* right) {
-        if (!left && right) return false;
-        if (left && !right) return false;
-        if (!left || !right) return left == right;
+    return isMirror(root->left, root->right);
+  }
 
-        return (left->val == right->val) &&
-               isMirror(left->left, right->right) &&
-               isMirror(left->right, right->left);
-    }
+  bool isMirror(TreeNode* left, TreeNode* right) {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+
+    return (left->val == right->val) && isMirror(left->left, right->right) &&
+           isMirror(left->right, right->left);
+  }
 };
 // @lc code=end
