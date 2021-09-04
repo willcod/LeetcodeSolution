3923e12b9c6d84af5a5e5ae42bb2317af9331801
willcod <william.wow@icloud.com>
2021-09-04 16:46:52 +0800

    合并两个有序数组

--- source/88.合并两个有序数组.cpp
+++ source/88.合并两个有序数组.cpp
@@ -6,13 +6,18 @@
 #include "cpp_includes.h"
 // @lc code=start
 class Solution {
-   public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        nums1.resize(m);
-        for (auto num : nums2)
-            nums1.push_back(num);
-        
-        sort(nums1.begin(), nums1.end());
+ public:
+  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n -1;
+
+    while (j>=0) {
+      if (i >= 0 && nums1[i] > nums2[j]) {
+          nums1[k--] = nums1[i--];
+      }else
+        nums1[k--] =  nums2[j--];
     }
+  }
 };
 // @lc code=end
