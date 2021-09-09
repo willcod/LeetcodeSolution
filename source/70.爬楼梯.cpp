7a76e283f4a92709c75fa1067b3e788e45362c9e
willcod <william.wow@icloud.com>
2021-09-09 09:21:25 +0800

    爬楼梯

--- source/70.爬楼梯.cpp
+++ source/70.爬楼梯.cpp
@@ -6,38 +6,17 @@
 #include "cpp_includes.h"
 // @lc code=start
 class Solution {
+    unordered_map<int, int> cache;
  public:
   int climbStairs(int n) {
-    if (n == 1) return 1;
+    if (n == 0 || n == 1) return 1;
     if (n == 2) return 2;
 
-    int one = 1;
-    int two = 2;
-    int sum = 0;
+    if (cache.count(n)) return cache[n];
 
-    for (int i = 3; i <= n; i++) {
-      sum = one + two;
-      one = two;
-      two = sum;
-    }
-
-    return sum;
+    auto re = climbStairs(n - 1) + climbStairs(n - 2);
+    cache[n] = re;
+    return re;
   }
 };
 // @lc code=end
-class Solution {
- public:
-  int climbStairs(int n) {
-    vector memo(n + 1, 0);
-    return climb(n, memo);
-  }
-
-  int climb(int n, vector<int>& memo) {
-    if (n == 0 || n == 1) return 1;
-    if (n == 2) return 2;
-    if (memo[n]) return memo[n];
-
-    int stairs = climb(n - 1, memo) + climb(n - 2, memo);
-    return memo[n] = stairs;
-  }
-};
