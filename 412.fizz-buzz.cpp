61f4f001ef00bd5dc5985b5d88bfe6091cd87bfd
willcod <william.wow@icloud.com>
2021-09-14 09:39:27 +0800

    move source code out of source folder and delete the source folder

---
+++ 412.fizz-buzz.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=412 lang=cpp
+ *
+ * [412] Fizz Buzz
+ */
+#include "cpp_includes.h"
+// @lc code=start
+class Solution {
+ public:
+  vector<string> fizzBuzz(int n) {
+    vector<string> result(n);
+
+    for (int i = 0; i < n; i++) {
+      if ((i + 1) % 15 == 0) {
+        result[i] = "FizzBuzz";
+      } else if ((i + 1) % 3 == 0) {
+        result[i] = "Fizz";
+      } else if ((i + 1) % 5 == 0) {
+        result[i] = "Buzz";
+      } else {
+        result[i] = to_string(i+1);
+      }
+    }
+    return result;
+  }
+};
+// @lc code=end
