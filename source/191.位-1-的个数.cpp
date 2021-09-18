408a1731b3e42ef2297fafc928ad3cedf3ee6e09
willcod <william.wow@icloud.com>
2021-09-18 10:50:44 +0800

    位1的个数

--- source/191.位-1-的个数.cpp
+++ source/191.位-1-的个数.cpp
@@ -8,12 +8,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int sum = 0;
-        while (n) {
-            n &= (n -1);
-            sum++;
+        int count = 0;
+        while (n > 0) {
+            if (n % 2) count++;
+            n /= 2;
         }
-        return sum;
+        return count;
     }
 };
 // @lc code=end
