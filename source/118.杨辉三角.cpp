9ea4b1bc208cf228cd0fd37cd77043fefa19f083
willcod <william.wow@icloud.com>
2021-09-22 10:19:53 +0800

    杨辉三角

--- source/118.杨辉三角.cpp
+++ source/118.杨辉三角.cpp
@@ -6,27 +6,26 @@
 #include "cpp_includes.h"
 // @lc code=start
 class Solution {
-   public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res;
+ public:
+  vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> res;
 
-        for (int i = 0; i < numRows; i++) {
-            if (i == 0) {
-                res.push_back({1});
-                continue;
-            }
+    for (int i = 0; i < numRows; i++) {
+      if (i == 0) {
+        res.push_back({1});
+        continue;
+      }
 
-            vector<int> level(i+1);
-            level[0] =1;
-            level[i] = 1;
-
-            for (int j = 1; j < i; j++) {
-                level[j] = res[i-1][j-1]+res[i-1][j];
-            }
-            res.push_back(level);
-        }
-
-        return res;
+      vector<int> level(i + 1);
+      level[0] = 1;
+      level[i] = 1;
+      for (int j = 1; j < i; j++) {
+        level[j] = res[i - 1][j - 1] + res[i - 1][j];
+      }
+      res.push_back(level);
     }
+
+    return res;
+  }
 };
 // @lc code=end
