be9d5f162234b3d6615d560bb36168395fdfbd87
willcod <william.wow@icloud.com>
2021-09-22 10:20:03 +0800

    颠倒二进制位

--- source/190.颠倒二进制位.cpp
+++ source/190.颠倒二进制位.cpp
@@ -6,13 +6,17 @@
 #include "cpp_includes.h"
 // @lc code=start
 class Solution {
-   public:
+public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t result = 0;
-        for (int i = 0; i < 32; i++)
-            result = (result << 1) + (n >> i & 1);
-        
+
+        for (int i = 0; i < 32 && n > 0; i++) {
+            result |= (n & 1) << (31 - i);
+            n >>= 1;
+        }
+
         return result;
     }
 };
 // @lc code=end
+
