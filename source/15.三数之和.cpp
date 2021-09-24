2068efed7805f1409009f8121c27d9c84f4d97a5
willcod <william.wow@icloud.com>
2021-09-24 21:16:48 +0800

    三数之和

--- source/15.三数之和.cpp
+++ source/15.三数之和.cpp
@@ -6,31 +6,38 @@
 #include "cpp_includes.h"
 // @lc code=start
 class Solution {
- public:
-  vector<vector<int>> threeSum(vector<int>& nums) {
-    vector<vector<int>> res;
-    sort(nums.begin(), nums.end());
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
 
-    for (int i = 0; i < nums.size(); i++) {
-      int f = i + 1;
-      int b = nums.size() - 1;
+        if (nums.size() < 3) return result;
 
-      while (f < b) {
-        int sum = nums[i] + nums[f] + nums[b];
-        if (sum == 0) {
-          res.push_back({nums[i], nums[f], nums[b]});
-          while (f < b && nums[f] == nums[f + 1]) f++;
-          while (f < b && nums[b] == nums[b - 1]) b--;
-          f++;
-          b--;
-        } else if (sum > 0)
-          b--;
-        else
-          f++;
-      }
-      while (i + 1 < nums.size() && nums[i + 1] == nums[i]) i++;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) break;
+
+            int j = i + 1;
+            int k = nums.size() - 1;
+
+            while (j < k) {
+                int sum = nums[i]+nums[j]+nums[k];
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[j], nums[k]});
+
+                    while (j < k && nums[j] == nums[j+1]) j++;
+                    while (j < k && nums[k] == nums[k-1]) k--;
+
+                    j++;
+                    k--;
+                } else if (sum > 0) k--;
+                else j++;
+            }
+
+            while (i+1 < nums.size() && nums[i] == nums[i+1]) i++;
+        }
+
+        return result;
     }
-    return res;
-  }
 };
 // @lc code=end
+
