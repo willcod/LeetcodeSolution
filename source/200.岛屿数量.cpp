de4a5478bc4cfe675688d0d866b15bcaefc141d9
willcod <william.wow@icloud.com>
2021-10-05 19:21:22 +0800

    岛屿数量

--- source/200.岛屿数量.cpp
+++ source/200.岛屿数量.cpp
@@ -6,38 +6,38 @@
 #include "cpp_includes.h"
 // @lc code=start
 class Solution {
-   public:
-    int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+ public:
+  int numIslands(vector<vector<char>>& grid) {
+    int m = grid.size();
+    int n = grid[0].size();
 
-        int count = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
-                    flood(grid, i, j);
-                }
-            }
+    int count = 0;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == '1') {
+          count++;
+          flood(grid, i, j);
         }
-        return count;
+      }
     }
 
-    void flood(vector<vector<char>>& grid, int x, int y) {
-        if (!isValid(grid, x, y)) return;
+    return count;
+  }
 
-        if (grid[x][y] == '1') {
-            grid[x][y] = '0';
+  void flood(vector<vector<char>>& grid, int x, int y) {
+    if (!isValid(grid, x, y)) return;
 
-            flood(grid, x - 1, y);
-            flood(grid, x + 1, y);
-            flood(grid, x, y - 1);
-            flood(grid, x, y + 1);
-        }
+    if (grid[x][y] == '1') {
+      grid[x][y] = '0';
+      flood(grid, x + 1, y);
+      flood(grid, x - 1, y);
+      flood(grid, x, y - 1);
+      flood(grid, x, y + 1);
     }
+  }
 
-    bool isValid(vector<vector<char>>& grid, int x, int y) {
-        return x >= 0 && y >= 0 && x < grid.size() && y < grid[0].size();
-    }
+  bool isValid(vector<vector<char>>& grid, int x, int y) {
+    return x >= 0 && y >= 0 && x < grid.size() && y < grid[0].size();
+  }
 };
 // @lc code=end
