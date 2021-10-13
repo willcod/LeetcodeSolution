d80e5bdd0fe53e62959425e0b9646a2059a94696
willcod <william.wow@icloud.com>
2021-10-13 21:03:53 +0800

    回溯

--- source/17.电话号码的字母组合.cpp
+++ source/17.电话号码的字母组合.cpp
@@ -6,31 +6,29 @@
 #include "cpp_includes.h"
 // @lc code=start
 class Solution {
- public:
-  string keyboard[10] = {"",    "",    "abc",  "def", "ghi",
-                         "jkl", "mno", "pqrs", "tuv", "wxyz"};
+public:
+    string keyboard[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        string str;
 
-  vector<string> letterCombinations(string digits) {
-    vector<string> res;
-    string str;
+        if (digits.empty()) return res;
 
-    if (digits.empty()) return res;
-
-    letterCombinations(res, str, digits, 0);
-    return res;
-  }
-
-  void letterCombinations(vector<string>& res, string str, string digits,
-                          int index) {
-    if (str.length() == digits.length()) {
-      res.push_back(str);
-      return;
+        letterCombinations(res, str, digits, 0);
+        return res;
     }
 
-    string keys = keyboard[digits[index]-'0'];
-    for (int i = 0; i < keys.length(); i++) {
-        letterCombinations(res, str+keys[i], digits, index+1);
+    void letterCombinations(vector<string>&res, string str, string digits, int index) {
+        if (str.length() == digits.length()) {
+            res.push_back(str);
+            return;
+        }
+
+        string keys = keyboard[digits[index] - '0'];
+        for (char c : keys) {
+            letterCombinations(res, str+c, digits, index+1);
+        }
     }
-  }
 };
 // @lc code=end
+
