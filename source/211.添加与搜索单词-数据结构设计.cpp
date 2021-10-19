0f89626b4728e0cd30122715572cbb934ed0fcee
willcod <william.wow@icloud.com>
2021-10-19 21:30:09 +0800

    daily

--- source/211.添加与搜索单词-数据结构设计.cpp
+++ source/211.添加与搜索单词-数据结构设计.cpp
@@ -6,48 +6,47 @@
 #include "cpp_includes.h"
 // @lc code=start
 class Trie {
-   public:
+    public: 
     bool is_word;
     Trie* children[26];
-
-    Trie() {
+    Trie() { 
         is_word = false;
         memset(children, 0, sizeof(children));
     }
 };
 
 class WordDictionary {
-   public:
-    /** Initialize your data structure here. */
-    WordDictionary() { root = new Trie(); }
-
-    /** Adds a word into the data structure. */
+    private: 
+    Trie *root;
+public:
+    WordDictionary() {
+        root = new Trie();
+    }
+    
     void addWord(string word) {
         auto node = root;
         for (char c : word) {
-            if (!node->children[c - 'a']) {
-                node->children[c - 'a'] = new Trie();
+            if (!node->children[c - 'a']){
+                node->children[c-'a'] = new Trie();
             }
             node = node->children[c - 'a'];
         }
-
         node->is_word = true;
     }
+    
+    bool search(string word) {
+        return search(word.c_str(), root);
+    }
 
-    /** Returns if the word is in the data structure. A word could contain the
-     * dot character '.' to represent any one letter. */
-    bool search(string word) { return search(word.c_str(), root); }
-
-   private:
     bool search(const char* word, Trie* node) {
-        for (int i = 0; word[i] && node; i++) {
+        for (int i = 0; word[i]&&node; i++) {
             if (word[i] != '.') {
                 node = node->children[word[i] - 'a'];
             } else {
                 auto tmp = node;
                 for (int j = 0; j < 26; j++) {
                     node = tmp->children[j];
-                    if (search(word + i + 1, node)) {
+                    if (search(word+i+1, node)){
                         return true;
                     }
                 }
@@ -55,9 +54,6 @@ class WordDictionary {
         }
         return node && node->is_word;
     }
-
-   private:
-    Trie* root;
 };
 
 /**
@@ -67,3 +63,4 @@ class WordDictionary {
  * bool param_2 = obj->search(word);
  */
 // @lc code=end
+
