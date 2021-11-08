9f2f4cc459dfbb64ddb43b97550410e21b8d2181
willcod <william.wow@icloud.com>
2021-11-08 21:53:09 +0800

    查找

--- source/268.丢失的数字.cpp
+++ source/268.丢失的数字.cpp
@@ -8,16 +8,12 @@
 class Solution {
  public:
   int missingNumber(vector<int>& nums) {
-    int n = nums.size();
-
     sort(nums.begin(), nums.end());
-    if (nums[0] != 0) return 0;
-    if (nums[n - 1] != n) return n;
-
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < nums.size(); i++) {
       if (nums[i] != i) return i;
     }
-    return -1;
+
+    return nums.size();
   }
 };
 // @lc code=end
